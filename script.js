2d9397353ac510f5b295117e20701713391d6b84
LucivaldoSalgado <lucivaldosalgado5@gmail.com>
2023-02-05 22:39:34 -0300

    adjust in image of  avatar

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function toggleMode() {
     // substituir a imagem
     if(html.classList.contains('light')) {
         //se tiver light mode, adicionar a imagem light
-        img.setAttribute("src","./assets/avatar-light.png")
+        img.setAttribute("src","./assets/avatar-light-1.png")
     } else {
         //se tiver sem light mode, manter a imagem normal
         img.setAttribute("src","./assets/avatar.png")
